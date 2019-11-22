32839c9f2e001af4c2d954cfc2e5b5c8b6e231a7
hokkaido <hello@kimchi.io>
2019-11-22 21:02:54 +0100

    Add copy generation

---
+++ infer-cnndm-copy.sh
@@ -0,0 +1,12 @@
+MODEL=checkpoints/checkpoint_best.pt
+DATADIR=datasets/cnndm-augmented-510/
+USERDIR=deps/MASS/MASS-summarization/mass
+
+fairseq-generate $DATADIR --path $MODEL \
+    --user-dir $USERDIR --task augmented_summarization_mass \
+    --batch-size 64 --beam 5 --min-len 50 --no-repeat-ngram-size 3 \
+    --skip-invalid-size-inputs-valid-test \
+    --embed-entities-encoder \
+    --copy-attn \
+    --lenpen 1.0 > output.txt
+
